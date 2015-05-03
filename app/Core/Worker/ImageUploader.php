033c935f29ac1dc1ebeac09a61dae72ceda0c533
Khaer Ansori <khaer.ansori@gmail.com>
2015-05-03 14:55:12 +0700

    Signed-off-by: Khaer Ansori <khaer.ansori@gmail.com>

--- app/Core/Worker/ImageUploader.php
+++ app/Core/Worker/ImageUploader.php
@@ -1,19 +1,12 @@
 <?php namespace App\Core\Worker;
 
-use GrahamCampbell\Flysystem\FlysystemManager;
-
 class ImageUploader
 {
-	protected $flysystem;
-	function __construct(FlysystemManager $flysystem) {
-		$this->flysystem = $flysystem;
-	}
-
 	public function fire($job, $data)
 	{
-		$this->flysystem->put($data['cloudPath'] . $data['filename'], $data['default']);
-	    $this->flysystem->put($data['cloudPath'] . "large_" . $data['filename'], $data['large']);
-	    $this->flysystem->put($data['cloudPath'] . "medium_" . $data['filename'], $data['medium']);
-	    $this->flysystem->put($data['cloudPath'] . "cart_" . $data['filename'], $data['cart']);
+		\Flysystem::put($data['cloudPath'] . $data['filename'], $data['default']);
+	    \Flysystem::put($data['cloudPath'] . "large_" . $data['filename'], $data['large']);
+	    \Flysystem::put($data['cloudPath'] . "medium_" . $data['filename'], $data['medium']);
+	    \Flysystem::put($data['cloudPath'] . "cart_" . $data['filename'], $data['cart']);
 	}
 }
