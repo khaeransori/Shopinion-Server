94c29ea24b6605d3bdbfb913739603479f4a1f89
Khaer Ansori <khaer.ansori@gmail.com>
2015-05-03 14:51:38 +0700

    Signed-off-by: Khaer Ansori <khaer.ansori@gmail.com>

--- app/Core/Worker/ImageUploader.php
+++ app/Core/Worker/ImageUploader.php
@@ -11,9 +11,9 @@ class ImageUploader
 
 	public function fire($job, $data)
 	{
-		$this->flysystem->put($data['cloudPath'] . $data['filename'], (string) $data['default']);
-	    $this->flysystem->put($data['cloudPath'] . "large_" . $data['filename'], (string) $data['large']);
-	    $this->flysystem->put($data['cloudPath'] . "medium_" . $data['filename'], (string) $data['medium']);
-	    $this->flysystem->put($data['cloudPath'] . "cart_" . $data['filename'], (string) $data['cart']);
+		$this->flysystem->put($data['cloudPath'] . $data['filename'], $data['default']);
+	    $this->flysystem->put($data['cloudPath'] . "large_" . $data['filename'], $data['large']);
+	    $this->flysystem->put($data['cloudPath'] . "medium_" . $data['filename'], $data['medium']);
+	    $this->flysystem->put($data['cloudPath'] . "cart_" . $data['filename'], $data['cart']);
 	}
 }
